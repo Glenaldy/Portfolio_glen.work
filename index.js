f74a441dfac35dd00794f7b9dc7f9a9a0c1b2061
Glenaldy <glenaldy98@yahoo.com>
2023-08-08 14:11:35 +0900

    fixed gmail new nodemailer

--- index.js
+++ index.js
@@ -99,20 +99,19 @@ app.get('/contact', function (req, res) {
 
 //FORM//
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
+    service:'gmail',
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASS
     }
 });
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take our messages');
-    }
-});
+// transporter.verify(function (error, success) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Server is ready to take our messages');
+//     }
+// });
 
 app.post('/send', (req, res) => {
     let form = new multiparty.Form();
